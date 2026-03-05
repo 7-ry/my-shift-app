8b5c5b27ee55aef2d1a25b2ecde44fb3a4aa7938
7-ry <ryushin0714@gmail.com>
2026-03-05 07:35:40 -0800

    APIfix

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,16 @@ import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore'; // ←★この1行を追加しました！
 
 // Your web app's Firebase configuration
+// src/firebase.js
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyCb0WrVDOLavj2nHPcOCf8gQzJKwf4GNaA',
-  authDomain: 'my-restaurant-shift.firebaseapp.com',
-  projectId: 'my-restaurant-shift',
-  storageBucket: 'my-restaurant-shift.firebasestorage.app',
-  messagingSenderId: '418889808365',
-  appId: '1:418889808365:web:f1d3607801be63c31c0fe4',
-  measurementId: 'G-NPVGPRMN4K',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
